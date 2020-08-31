8e24a48de37dab6b78e44bc3ef56723e4ae11e5f
Isaque Costa <isaquebc@gmail.com>
2020-08-31 16:10:53 -0300

    Dfoi

--- index.js
+++ index.js
@@ -14,17 +14,20 @@ console.log('clients:', clients)
 
 io.on('connection', (socket) => {
   socket.on('room/set-visitant', ({ stream_id }) => {
+    console.log(' => room/set-visitant')
     clients.push({ socket_id: socket.id, stream_id });
     socket.emit("room/update", clients.filter(st => st.socket_id !== socket.id));
   });
   socket.on("room/signal", payload => {
-    console.log('\n\nroom/signal\n\n\n\n')
+    console.log(' => room/signal')
     io.to(payload.userToSignal).emit('room/user-joined', { signal: payload.signal, callerID: payload.callerID });
   });
   socket.on("room/signal-back", payload => {
+    console.log(' => room/signal-back')
     io.to(payload.callerID).emit('room/signal-answer-back', { signal: payload.signal, id: socket.id });
   });
   socket.on('disconnect', () => {
+    console.log(' => disconnect')
     clients = clients.filter(st => st.socket_id !== socket.id);
   });
 });
