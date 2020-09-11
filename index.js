f586ebd59542fe2f058b0c3ca97fcd2f998adca4
Isaque Costa <isaquebc@gmail.com>
2020-09-11 17:17:13 -0300

    updating

--- index.js
+++ index.js
@@ -13,21 +13,31 @@ app.get('/', (req, res) => {
 console.log('clients:', clients)
 
 io.on('connection', (socket) => {
+  console.log(' => connect')
+  console.count('step')
+  console.log('----------------')
   socket.on('room/set-visitant', ({ stream_id }) => {
-    console.log(' => room/set-visitant')
+    console.log('----------------')
     clients.push({ socket_id: socket.id, stream_id });
     socket.emit("room/update", clients.filter(st => st.socket_id !== socket.id));
   });
   socket.on("room/signal", payload => {
     console.log(' => room/signal')
+    console.log('payload:', payload)
+    console.count('step')
+    console.log('----------------')
     io.to(payload.userToSignal).emit('room/user-joined', { signal: payload.signal, callerID: payload.callerID });
   });
   socket.on("room/signal-back", payload => {
     console.log(' => room/signal-back')
+    console.count('step')
+    console.log('----------------')
     io.to(payload.callerID).emit('room/signal-answer-back', { signal: payload.signal, id: socket.id });
   });
   socket.on('disconnect', () => {
     console.log(' => disconnect')
+    console.count('step')
+    console.log('----------------')
     clients = clients.filter(st => st.socket_id !== socket.id);
   });
 });
