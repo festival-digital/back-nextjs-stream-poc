3ee7a13d29de40f52cc73596c84e0ff11333573c
Isaque Costa <isaquebc@gmail.com>
2020-10-23 11:29:21 -0300

    .

--- db/Mongodb.js
+++ db/Mongodb.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
-const person = require('./schemas/person.model');
-const room = require('./schemas/room.model');
+// const mongoose = require('mongoose');
+// const person = require('./schemas/person.model');
+// const room = require('./schemas/room.model');
 
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
 module.exports = async function ({ conn, mongoUrl = 'mongodb+srv://stream:X2XxXQzIFHA7PlKJ@feiradigital.wqkom.mongodb.net/feiradigital?retryWrites=true&w=majority' }) {
-  console.log('mongoUrl: ', mongoUrl);
-  console.log('mongodb+srv://stream:jTKBFXzkQvheO743@feiradigital.wqkom.mongodb.net/feiradigital?retryWrites=true&w=majority');
-  try {
-    console.log('=> using new database connection');
+  // console.log('mongoUrl: ', mongoUrl);
+  // console.log('mongodb+srv://stream:jTKBFXzkQvheO743@feiradigital.wqkom.mongodb.net/feiradigital?retryWrites=true&w=majority');
+  // try {
+  //   console.log('=> using new database connection');
 
-    const newConnection = await mongoose.createConnection(mongoUrl, {
-      bufferCommands: false,
-      bufferMaxEntries: 0,
-      keepAlive: true,
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+  //   const newConnection = await mongoose.createConnection(mongoUrl, {
+  //     bufferCommands: false,
+  //     bufferMaxEntries: 0,
+  //     keepAlive: true,
+  //     useUnifiedTopology: true,
+  //     useNewUrlParser: true,
+  //   });
 
-    newConnection.model('watcher', person);
-    newConnection.model('room', room);
-    return newConnection;
-  } catch (err) {
-    console.log('error: ', [err]);
-    throw err;
-  }
+  //   newConnection.model('watcher', person);
+  //   newConnection.model('room', room);
+  //   return newConnection;
+  // } catch (err) {
+  //   console.log('error: ', [err]);
+  //   throw err;
+  // }
 };
